72c2414914585fc3bdb2010be45b71cc515149a9
Anonymous Coward <foo@bar.bra>
2017-09-18 12:12:00 +0000

    language html + json

--- translate.sh
+++ translate.sh
@@ -2,7 +2,7 @@
 
 ginger index.tpl lang/en.json | grep -v "^null$" > index.html
 
-for lang in en
+for lang in zh ja ko en de
 do  
   ginger index.tpl lang/$lang.json | grep -v "^null$" > index_$lang.html
 done
